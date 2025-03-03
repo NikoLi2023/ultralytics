5177e247bb6c075cff12dcfd205ecf5f19b19d48
Niko Li <42465628+NikoLi2023@users.noreply.github.com>
2025-03-03 17:05:10 +0800

    updated Encrypt Codes;

--- ClassficationDemo.py
+++ ClassficationDemo.py
@@ -1,10 +1,36 @@
 from ultralytics import YOLO
+import clr
+import os
+
+# sys.path.append(".")
+clr.AddReference("ModelCrypto")
+from ModelCrypto import *
 
 if __name__ == '__main__':
     # freeze_support()
     # Load a model
-    model = YOLO("yolo11n-cls.pt")  # load a pretrained model (recommended for training)
+    # model = YOLO("yolo11n-cls.pt")  # load a pretrained model (recommended for training)
+    # # Train the models
+    # results = model.train(data=r"C:\Users\liyun\Desktop\PythonYolov12\datasets\cifar10", epochs=2, imgsz=32)
+    # success = model.export(format="onnx")
+    # print(success)
+    # # get the folder path of onnx;
+    # onnxPath = os.path.dirname(success)
+    # ModelCrypto.EncryptFolder(onnxPath)
+    # print("complete!")
 
-    # Train the models
-    results = model.train(data=r"C:\Users\liyun\Desktop\PythonYolov12\datasets\cifar10", epochs=2, imgsz=32)
-    model.export(format="onnx")
+
+    modelPath1= r"D:\WorkRepository\ultralytics\runs\classify\train2\weights\best.gqpt"
+
+    if modelPath1.endswith(".gqpt"):
+        #判断是否存在Cache文件夹，不存在则创建；
+        if not os.path.exists("Cache"):
+            os.makedirs("Cache")
+        modelParserPath="./Cache/trainTemp.pt"
+        succeed=ModelCrypto.DecryptFunc(modelPath1,modelParserPath)
+        if succeed:
+            print("Complete!")
+        else:
+            print("Decrypt fail!")
+
+
